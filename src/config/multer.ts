0c490ba278a2c40003dd63aac831769a9069ac24
amarudaicz <amarudaicz10@gmail.com>
2023-12-27 18:01:15 -0300

    s

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -4,7 +4,8 @@ import path from 'path';
 // Configurar Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Directorio donde se guardarán los archivos
+    console.log(path.dirname);
+    cb(null, '../uploads/'); // Directorio donde se guardarán los archivos
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname); // Nombre del archivo
