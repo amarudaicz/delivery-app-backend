0f4bf08b20e181a3a2f281e3c1f332550ae3f782
amarudaicz <amarudaicz10@gmail.com>
2023-12-27 19:50:12 -0300

    a

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -4,30 +4,12 @@ import path from 'path';
 // Configurar Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log();
     cb(null, 'uploads/'); // Directorio donde se guardarán los archivos
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname); // Nombre del archivo
   }
-}); 
+});
 
 const upload = multer({ storage: storage });
 export default upload;
-
-// logFolderStructure.js
-
-// function logRootFolders(rootPath:any) {
-//   const items = fs.readdirSync(rootPath);
-
-//   const folders = items.filter(item => {
-//     const itemPath = path.join(rootPath, item);
-//     return fs.statSync(itemPath).isDirectory();
-//   });
-
-//   console.log(`Root Folders for ${rootPath}:`);
-//   console.log(folders.join('\n'));
-// }
-
-// const rootFolder = path.resolve('./'); // Ruta absoluta de la carpeta raíz del script
-// logRootFolders(rootFolder);
